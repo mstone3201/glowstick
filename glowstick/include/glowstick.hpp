4043d36c321c571e07f7545bb958369d26462c0a
mstone3201 <mbs3201@gmail.com>
2024-04-24 18:11:55 -0400

    Setup
    Added vcpkg dependencies and set up CMake to work with the project layout

---
+++ glowstick/include/glowstick.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace glowstick {
+    void hello();
+}
---
+++ glowstick/src/glowstick.cpp
@@ -0,0 +1,13 @@
+#include "glowstick.hpp"
+
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+
+namespace glowstick {
+    void hello() {
+        std::cout << "Hello world!" << std::endl;
+    }
+}
